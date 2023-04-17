8671c5fafe0137844ecfe56a99ec493a727e371d
andyjjrt <andyjjrt@gmail.com>
2023-04-17 14:42:31 +0800

    feat: add theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,4 +1,32 @@
-import "vuetify/styles";
-import { createVuetify } from "vuetify";
+import "../css/main.scss";
+import { ThemeDefinition, createVuetify } from "vuetify";
 
-export default createVuetify();
+const lightTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    primary: "#7986CB",
+    "primary-darken-1": "#5C6BC0",
+    secondary: "#00897B",
+    "secondary-darken-1": "#00796B",
+  },
+};
+
+const darkTheme: ThemeDefinition = {
+  dark: true,
+  colors: {
+    primary: "#5C6BC0",
+    "primary-darken-1": "#3949AB",
+    secondary: "#00796B",
+    "secondary-darken-1": "#00695C",
+  },
+};
+
+export default createVuetify({
+  theme: {
+    defaultTheme: "lightTheme",
+    themes: {
+      lightTheme,
+      darkTheme,
+    },
+  },
+});
