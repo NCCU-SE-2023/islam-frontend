9c13c29c834523aaaea12000781da48c66779d42
roychshao <roych.shao@gmail.com>
2023-04-18 19:06:22 +0800

    feat: simple login and register card

--- src/components/LoginCard.vue
+++ src/components/LoginCard.vue
@@ -1,3 +1,35 @@
 <template>
-  Login
+    <v-form @submit.prevent="submitForm">
+        <v-text-field
+            v-model="name"
+            label="Name"
+            required
+        ></v-text-field>
+        <v-text-field
+            v-model="password"
+            label="Password"
+            type="password"
+            required
+            :rules="[passwordRules]"
+        ></v-text-field>
+        <v-btn type="submit" color="primary">Submit</v-btn>
+    </v-form>
 </template>
+
+<script setup lang="ts">
+import { ref } from "vue"
+
+const name = ref("");
+const password = ref("");
+
+const passwordRules = [
+  v => (v && v.length >= 8) || 'Password must be greater than 8 characters'
+]
+
+const submitForm = () => {
+    console.log(name.value);
+    console.log(password.value);
+}
+
+
+</script>
