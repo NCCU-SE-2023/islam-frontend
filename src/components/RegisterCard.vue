81b181c0e234db100dca6ad5f0cd9e683f7474ac
roychshao <roych.shao@gmail.com>
2023-04-19 15:46:52 +0800

    feat: update Login page style

--- src/components/RegisterCard.vue
+++ src/components/RegisterCard.vue
@@ -1,20 +1,35 @@
 <template>
   <v-form @submit.prevent="submitForm">
-    <v-text-field v-model="name" label="Name" required></v-text-field>
-    <v-text-field
-      v-model="email"
-      label="Email"
-      type="email"
-      required
-    ></v-text-field>
-    <v-text-field
-      v-model="password"
-      label="Password"
-      type="password"
-      required
-      :rules="[passwordRules]"
-    ></v-text-field>
-    <v-btn type="submit" color="primary">Submit</v-btn>
+    <v-col class="pl-5 pr-5 pb-0">
+      <v-text-field
+        v-model="name"
+        label="Name"
+        variant="underlined"
+        required
+      ></v-text-field>
+    </v-col>
+    <v-col class="pl-5 pr-5 pb-0">
+      <v-text-field
+        v-model="email"
+        label="Email"
+        type="email"
+        variant="underlined"
+        required
+      ></v-text-field>
+    </v-col>
+    <v-col class="pl-5 pr-5 pb-0">
+      <v-text-field
+        v-model="password"
+        label="Password"
+        type="password"
+        variant="underlined"
+        required
+        :rules="[passwordRules]"
+      ></v-text-field>
+    </v-col>
+    <v-col class="d-flex justify-center pb-3">
+      <v-btn type="submit" color="primary">Submit</v-btn>
+    </v-col>
   </v-form>
 </template>
 
@@ -25,9 +40,10 @@ const name = ref("");
 const email = ref("");
 const password = ref("");
 
-const passwordRules = [
-  (v) => (v && v.length >= 8) || "Password must be greater than 8 characters",
-];
+const passwordRules = {
+  password: (v: string) =>
+    (v && v.length >= 8) || "Password must be greater than 8 characters",
+};
 
 const submitForm = () => {
   console.log(name.value);
